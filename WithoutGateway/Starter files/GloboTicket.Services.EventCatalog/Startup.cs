a59773e18c315e0ff7a1681a3a28ee27567c3fde
Steve Beckert <sbeckert@beckman.com>
2026-02-24 19:03:24 -0800

    SYFM-bec Demo Blocking Access to a Microservice
    ApplicationException as expected

--- WithoutGateway/Starter files/GloboTicket.Services.EventCatalog/Startup.cs
+++ WithoutGateway/Starter files/GloboTicket.Services.EventCatalog/Startup.cs
@@ -10,8 +10,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Net.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Polly;
 using Polly.Extensions.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace GloboTicket.Services.EventCatalog
 {
@@ -34,11 +37,26 @@ namespace GloboTicket.Services.EventCatalog
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-            services.AddControllers();
+            var requireAuthenticatedUserPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+
+            services.AddControllers( configure =>
+            {
+                configure.Filters.Add(new AuthorizeFilter(requireAuthenticatedUserPolicy));
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "EventDto Catalog API", Version = "v1" });
             });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => 
+                { 
+                    options.Authority = "https://localhost:5010";
+                    options.Audience = "globoticket";
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -59,6 +77,8 @@ namespace GloboTicket.Services.EventCatalog
             });
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
